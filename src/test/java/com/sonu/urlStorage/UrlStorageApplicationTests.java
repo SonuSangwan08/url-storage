8448046330aa8859cabd08a1af6862bc4ffa13ec
Sonu Sangwan <sonusangwan.asiwas@gmail.com>
2022-08-17 23:31:59 +0530

    added initial spring boot structural changes

---
+++ src/test/java/com/sonu/urlStorage/UrlStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonu.urlStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
